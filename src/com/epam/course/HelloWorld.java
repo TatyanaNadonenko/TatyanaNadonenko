50e8115791138e10b69b9c41d588d3b439d379a8
Tatyana <tanyanadoneko@gmail.com>
2023-03-10 19:00:58 +0300

    [001] HelloWorld created

---
+++ src/com/epam/course/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.epam.course;
+
+/**
+ * This is the code of simple application
+ * to print "Hello World!" to standard output.
+ *
+ * @author Tatyana Nadonenko
+ * @version 1.0
+ * @since 10-03-2023
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
